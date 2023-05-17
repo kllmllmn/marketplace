155dc6b0ba9798676e3d53b53b67adc2dcd700c6
szh <1877308253@qq.com>
2023-05-17 23:20:27 +0800

    background animation

--- src/assets/menuList.js
+++ src/assets/menuList.js
@@ -2,27 +2,27 @@ import { nanoid } from "nanoid";
 // 使用nanoid会导致热更新失效
 export const menuList = [
   {
-    id: 0,
+    id: "0",
     name: "home",
     path: "home",
     text: "首页",
     children: [],
   },
   {
-    id: 1,
+    id: "1",
     name: "echarts",
     path: "echarts",
     text: "图表",
     children: [],
   },
   {
-    id: 2,
+    id: "2",
     name: "videos",
     path: "videos",
     text: "影视",
     children: [
       {
-        id: 2 - 0,
+        id: "2-0",
         name: "animation",
         path: "videos/animation",
         text: "动画",
@@ -31,13 +31,13 @@ export const menuList = [
     ],
   },
   {
-    id: 3,
+    id: "3",
     name: "gallery",
     path: "gallery",
     text: "画廊",
     children: [
       {
-        id: 3 - 0,
+        id: "3-0",
         name: "gallery/preview",
         path: "gallery/preview",
         text: "预览",
@@ -46,33 +46,33 @@ export const menuList = [
     ],
   },
   {
-    id: 4,
+    id: "4",
     name: "editor",
     path: "editor",
     text: "编辑器",
     children: [],
   },
   {
-    id: 5,
+    id: "5",
     name: "music",
     path: "music",
     text: "音乐",
     children: [
       {
-        id: 5 - 0,
+        id: "5-0",
         name: "music/kwDownload",
         path: "music/kwDownload",
         text: "kwDownload",
         children: [
           {
-            id: 5 - 0 - 0,
+            id: "5-0-0",
             name: "music/kwDownload/2023",
             path: "music/kwDownload/2023",
             text: "2023",
             children: [],
           },
           {
-            id: 5 - 0 - 1,
+            id: "5-0-1",
             name: "music/kwDownload/test01",
             path: "music/kwDownload/test01",
             text: "test01",
@@ -83,13 +83,13 @@ export const menuList = [
     ],
   },
   {
-    id: 6,
+    id: "6",
     name: "tools",
     path: "tools",
     text: "工具集",
     children: [
       {
-        id: 6 - 0,
+        id: "6-0",
         name: "tools/e-signature",
         path: "tools/e-signature",
         text: "电子签名",
@@ -98,16 +98,31 @@ export const menuList = [
     ],
   },
   {
-    id: 7,
+    id: "7",
     name: "table",
     path: "table",
     text: "表格",
     children: [
       {
-        id: 7 - 0,
+        id: "7-0",
         name: "table/common",
         path: "table/common",
         text: "普通表格",
+        children: [],
+      },
+    ],
+  },
+  {
+    id: "8",
+    name: "css",
+    path: "css",
+    text: "css",
+    children: [
+      {
+        id: " 8-0",
+        name: "css/1",
+        path: "css/1",
+        text: "css1",
         children: [],
       },
     ],
---
+++ src/views/css/1/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrap flex-center">
+    <h1>background animation</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="less">
+.wrap {
+  background-image: linear-gradient(
+    135deg,
+    rgb(189, 106, 178),
+    rgb(135, 106, 189),
+    rgb(106, 186, 189),
+    rgb(181, 189, 106),
+    rgb(189, 116, 106)
+  );
+  background-size: 400%;
+  height: 100%;
+  animation: bgmove 20s infinite;
+}
+@keyframes bgmove {
+  0% {
+    background-position: 0% 0%;
+  }
+  50% {
+    background-position: 100% 100%;
+  }
+  100% {
+    background-position: 0% 0%;
+  }
+}
+</style>
