de69bbb8d9fd7d7c779dc8131ee42061a40aa7e2
szh <1877308253@qq.com>
2023-11-01 23:34:47 +0800

    chore: 低版本兼容

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ import { VantResolver } from "@vant/auto-import-resolver";
 import Markdown from "vite-plugin-vue-markdown";
 import MarkdownItAnchor from "markdown-it-anchor";
 import MarkdownItPrism from "markdown-it-prism";
+import legacy from "@vitejs/plugin-legacy";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -40,6 +41,30 @@ export default defineConfig({
       },
       // Class names for the wrapper div
       wrapperClasses: "markdown-body",
+    }),
+    legacy({
+      targets: ["defaults", "ie >= 11", "chrome 52"], //需要兼容的目标列表，可以设置多个
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+      renderLegacyChunks: true,
+      polyfills: [
+        "es.symbol",
+        "es.array.filter",
+        "es.promise",
+        "es.promise.finally",
+        "es/map",
+        "es/set",
+        "es.array.for-each",
+        "es.object.define-properties",
+        "es.object.define-property",
+        "es.object.get-own-property-descriptor",
+        "es.object.get-own-property-descriptors",
+        "es.object.keys",
+        "es.object.to-string",
+        "es.object.from-entries",
+        "web.dom-collections.for-each",
+        "esnext.global-this",
+        "esnext.string.match-all",
+      ],
     }),
   ],
   lintOnSave: false,
